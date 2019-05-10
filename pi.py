b0088edfba73438845dcae6ea4a2cc87562c3939
Chris Richardson <chris@bpi.cam.ac.uk>
2019-05-10 13:09:42 +0100

    Add function and run several times

--- pi.py
+++ pi.py
@@ -1,9 +1,16 @@
-a = 2.0
-nmax = 100000
-for n in range(1, nmax):
-    a = a * (n*n)/(n*n - 0.25)
-print(a)
-print("QED - which should be close to pi")
+import math
+
+def pi(nmax):
+    a = 2.0
+    for n in range(1, nmax):
+        a = a * (n*n)/(n*n - 0.25)
+    return a
+
+for n in range(7):
+    nmax = 10**n
+    print (nmax, pi(nmax), math.pi - pi(nmax))
+
+
 
 
 
